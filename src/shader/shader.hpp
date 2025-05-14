f9fe739675d661890df9196049c381977fc69fce
Nitesh Kumar <devniteshkumar96@gmail.com>
2025-05-14 20:38:56 +0530

    Live Hot-Reloading

--- src/shader/shader.hpp
+++ src/shader/shader.hpp
@@ -20,6 +20,7 @@ public:
     void setMat4(const std::string& name, glm::mat4 value) const;
     void setVec3(const std::string& name, float x, float y, float z) const;
     void setVec3(const std::string& name, glm::vec3 vector) const;
+    void reload(Shader *shader, const char* vertexPath, const char* fragmentPath);
 };
 
 #endif
